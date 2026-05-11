21ba17b996b9f103de980f18db2b2dfe0db0521e
Nakame-Akame <brayantinocooscanoa@gmail.com>
2026-05-10 20:50:16 -0500

    feat: agregar autenticacion

---
+++ js/auth.js
@@ -0,0 +1,77 @@
+const registerForm =
+document.getElementById("registerForm");
+
+if(registerForm){
+
+registerForm.addEventListener("submit",(e)=>{
+
+e.preventDefault();
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(username === "" || password === ""){
+alert("Complete los campos");
+return;
+}
+
+const user = {
+username,
+password
+};
+
+localStorage.setItem(
+"user",
+JSON.stringify(user)
+);
+
+alert("Usuario registrado");
+
+window.location.href = "login.html";
+
+});
+
+}
+const loginForm =
+document.getElementById("loginForm");
+
+if(loginForm){
+
+loginForm.addEventListener("submit",(e)=>{
+
+e.preventDefault();
+
+const username =
+document.getElementById("loginUser").value;
+
+const password =
+document.getElementById("loginPass").value;
+
+const savedUser =
+JSON.parse(localStorage.getItem("user"));
+
+if(
+savedUser &&
+savedUser.username === username &&
+savedUser.password === password
+){
+
+localStorage.setItem(
+"session",
+"active"
+);
+
+window.location.href = "index.html";
+
+}else{
+
+alert("Credenciales incorrectas");
+
+}
+
+});
+
+}
